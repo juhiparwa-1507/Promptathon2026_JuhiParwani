6f273cc00717b344fefe3741dde3a5a02df4a8f6
Juhi Parwani <juhiparwani65@gmail.com>
2026-08-06 16:19:48 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,35 +1,16 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-app = FastAPI(title="Smart Hospital Command Center")
-
-
-class Patient(BaseModel):
-    name: str
-    age: int
-    symptoms: str
-
+app = FastAPI(
+    title="Smart Hospital Command Center API"
+)
 
 @app.get("/")
 def home():
     return {
-        "message": "Smart Hospital Backend Running"
-    }
-
-
-@app.post("/patient")
-def patient_analysis(patient: Patient):
-
-    if "chest pain" in patient.symptoms.lower():
-        risk = "Critical"
-        score = 100
-    else:
-        risk = "Moderate"
-        score = 50
-
-    return {
-        "patient_name": patient.name,
-        "risk_level": risk,
-        "risk_score": score,
-        "agent": "Reception Agent"
+        "message": "Backend API Running",
+        "agents": [
+            "Reception Agent",
+            "Doctor Agent",
+            "Memory Agent"
+        ]
     }
